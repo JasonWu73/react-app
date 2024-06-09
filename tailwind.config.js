3475bd04b717a4703d311b7a1523b10cf214a26d
吴仙杰 <wuxianjiezh@gmail.com>
2024-06-09 23:39:00 +0800

    UI 配置

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,31 @@
-/** @type {import("tailwindcss").Config} */
+import typography from '@tailwindcss/typography';
+import animatePlugin from 'tailwindcss-animate';
+import daisyui from 'daisyui';
+
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
   ],
-  theme: { extend: {} },
-  plugins: []
+
+  theme: {
+    // 设置容器默认居中
+    container: {
+      center: true
+    },
+    extend: {
+      height: {
+        // 考虑手机浏览器的地址栏
+        screen: ['100vh', '100dvh']
+      }
+    }
+  },
+
+  daisyui: {
+    themes: ['emerald', 'dracula']
+  },
+
+  plugins: [typography, animatePlugin, daisyui]
 };
 
